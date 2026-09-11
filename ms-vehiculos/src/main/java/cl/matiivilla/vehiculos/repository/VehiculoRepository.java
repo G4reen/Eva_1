791b59d2b34ff8f6c40c740bbc2337bdb17286ee
g4reen <ba.sepulvedat@duocuc.cl>
2026-09-11 04:07:55 -0300

    feat: agrega endpoint para listar vehículos disponibles

--- ms-vehiculos/src/main/java/cl/matiivilla/vehiculos/repository/VehiculoRepository.java
+++ ms-vehiculos/src/main/java/cl/matiivilla/vehiculos/repository/VehiculoRepository.java
@@ -2,7 +2,7 @@ package cl.matiivilla.vehiculos.repository;
 
 import cl.matiivilla.vehiculos.model.Vehiculo;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 import java.util.Optional;
 
 public interface VehiculoRepository extends JpaRepository<Vehiculo, Long> {
@@ -11,3 +11,4 @@ public interface VehiculoRepository extends JpaRepository<Vehiculo, Long> {
 
     Optional<Vehiculo> findByPatente(String patente);
 }
+List<Vehiculo> findByDisponibleTrue();
